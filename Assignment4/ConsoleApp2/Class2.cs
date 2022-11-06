39e5d820f9a17d2c6c09647b1e9a2ac9cb26b7e7
knagasushma <115627695+knagasushma@users.noreply.github.com>
2022-11-06 13:31:21 +0000

    Add files via upload

---
+++ Assignment4/ConsoleApp2/Class2.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class2
+    {
+
+        class MyStack
+        {
+            private int[] ele;
+            private int top;
+            private int max;
+            public MyStack(int size)
+            {
+                ele = new int[size];
+                top = -1;
+                max = size;
+
+            }
+            public void Push(int item)
+            {
+                try
+                {
+                    ele[++top] = item;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                }
+            }
+            public int Pop()
+            {
+                Console.WriteLine("Poped element is:" + ele[top]);
+                return ele[top--];
+            }
+            public void PrintStack()
+            {
+
+                if (top == -1)
+                {
+                    Console.WriteLine("Stack is Empty");
+                    return;
+
+
+                }
+                else
+                {
+                    for (int i = 0; i <= top; i++)
+                    {
+                        Console.WriteLine("Item[" + (i + 1) + "]:" + ele[i]);
+
+                    }
+                }
+            }
+            class Program
+            {
+                static void Main()
+                {
+                    StackException se = new StackException();
+                    se.Method();
+
+                    Console.ReadLine();
+                }
+                class StackException
+                {
+                    public void Method()
+                    {
+                        MyStack s = new MyStack(5);
+                        s.Push(10);
+                        s.Push(20);
+                        s.Push(30);
+                        s.Push(40);
+                        s.Push(50);
+                        Console.WriteLine("Items are : ");
+                        s.PrintStack();
+                        s.Pop();
+                        s.PrintStack();
+                        s.Push(50);
+                        s.Push(60);
+
+                    }
+                }
+
+            }
+
+
+        }
+
+    }
+}
+
+
+            
+        
+    
+
+
---
+++ Assignment4/ConsoleApp2/Program.cs
@@ -0,0 +1,141 @@
+﻿namespace Assignment3
+{
+    public interface Iprintable
+    {
+        void Print();
+    }
+
+
+    public class EmployeeManagementSystem : Iprintable
+    {
+
+
+        public int Salary;
+
+        public void Print()
+        {
+
+
+
+            Console.WriteLine("enter the first name of the employee:");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("enter last name of the employee:");
+            string LastName = Console.ReadLine();
+            Console.WriteLine("Enter employee Salary :");
+            Salary = Convert.ToInt32(Console.ReadLine());
+            if (Salary <= 0 || Salary > 20000)
+            {
+                Console.WriteLine("please enter a valid value again from starting....THANK YOU");
+
+                Print();
+            }
+            else if (Salary < 5000)
+            {
+                Console.WriteLine("Employee Name : {0}", FirstName + LastName);
+                Console.WriteLine("Net Salary : {0}", Salary);
+
+                int PF = 8 * (Salary / 100);
+                Console.WriteLine("Gross Salary : {0}", Salary + PF);
+            }
+            else if (Salary > 5000 && Salary < 10000)
+            {
+                Console.WriteLine("Employee Name : {0}", FirstName + LastName);
+                Console.WriteLine("Net Salary : {0}", Salary);
+
+                int PF = 9 * (Salary / 100);
+                Console.WriteLine("Gross Salary : {0}", Salary + PF);
+            }
+            else if (Salary > 10000 && Salary < 20000)
+            {
+                Console.WriteLine("Employee Name : {0}", FirstName + LastName);
+                Console.WriteLine("Net Salary : {0}", Salary);
+
+                int PF = 10 * (Salary / 100);
+                Console.WriteLine("Gross Salary : {0}", Salary + PF);
+            }
+
+
+
+
+        }
+        public class Manager : EmployeeManagementSystem
+        {
+            private double PetrolAllowance;
+            private double FoodAllowance;
+            private double OtherAllowance;
+
+
+
+
+            public void Calculate()
+            {
+                Console.WriteLine("Enter Salary of Manager:");
+                Salary = Convert.ToInt32(Console.ReadLine());
+                PetrolAllowance = 8 * (Salary / 100);
+                FoodAllowance = 13 * (Salary / 100);
+                OtherAllowance = 3 * (Salary / 100);
+
+                Console.WriteLine("GROSS SALARY OF MANAGER = {0}", Salary + PetrolAllowance + FoodAllowance + OtherAllowance);
+            }
+            public void CalculateSalary()
+            {
+                Console.WriteLine("NET SALARY OF MANAGER = {0}", Salary);
+            }
+
+
+        }
+        public class MarketingExecutive : EmployeeManagementSystem
+        {
+            private int KiloMeterTravel;
+            private int TourAllowances;
+            private int TelephoneAllowances;
+
+
+
+
+            public void Calculate()
+            {
+                Console.WriteLine("Enter Marketing Executive Salary:");
+                Salary = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter kilomter travel by Marketing EXecutive:");
+                KiloMeterTravel = Convert.ToInt32(Console.ReadLine());
+                TourAllowances = KiloMeterTravel * 5;
+                TelephoneAllowances = 1000;
+
+                Console.WriteLine("Gross Salary of marketing executive: {0}", Salary + KiloMeterTravel + TourAllowances + TelephoneAllowances);
+            }
+            public void CalculateSalary()
+            {
+                Console.WriteLine("Net salary of marketing exective:{0}", +Salary);
+            }
+        }
+
+
+
+
+        public class Q1
+        {
+            public static void Main()
+            {
+
+
+
+                Iprintable IP = new EmployeeManagementSystem();
+                IP.Print();
+
+
+                Manager manager = new Manager();
+                manager.Calculate();
+                manager.CalculateSalary();
+
+
+                MarketingExecutive ME = new MarketingExecutive();
+
+                ME.Calculate();
+                ME.CalculateSalary();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
